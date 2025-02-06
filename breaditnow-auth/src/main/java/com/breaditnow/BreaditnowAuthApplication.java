7db34c304d33b31c5aadcab3a71d59210d2f734a
pug9483 <2019136060@koreatech.ac.kr>
2025-02-07 00:56:21 +0900

    feat : 각 모듈별로 application.yml을 적용하여 올바르게 동작하는지 확인한다 (#1)

---
+++ breaditnow-auth/src/main/java/com/breaditnow/BreaditnowAuthApplication.java
@@ -0,0 +1,11 @@
+package com.breaditnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreaditnowAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BreaditnowAuthApplication.class, args);
+    }
+}
---
+++ breaditnow-mysql/src/main/java/com/breaditnow/BreaditnowMysqlApplication.java
@@ -0,0 +1,11 @@
+package com.breaditnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreaditnowMysqlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BreaditnowMysqlApplication.class, args);
+    }
+}
