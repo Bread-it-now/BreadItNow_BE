d57e2d9e305f89278740fe76180ee8f313f5bd4f
pug9483 <2019136060@koreatech.ac.kr>
2025-02-08 00:09:07 +0900

    feat : mysql 모듈 분리 (#1)

--- breaditnow-customer-api/src/main/java/com/breaditnow/customer/BreaditnowCustomerApplication.java
+++ breaditnow-customer-api/src/main/java/com/breaditnow/customer/BreaditnowCustomerApplication.java
@@ -1,9 +1,11 @@
-package com.breaditnow;
+package com.breaditnow.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.breaditnow.customer", "com.breaditnow.mysql"})
+@EnableJpaRepositories(basePackages = "com.breaditnow.mysql.repository")
 public class BreaditnowCustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BreaditnowCustomerApplication.class, args);
--- breaditnow-mysql/src/main/java/com/breaditnow/mysql/BreaditnowMysqlApplication.java
+++ breaditnow-mysql/src/main/java/com/breaditnow/mysql/BreaditnowMysqlApplication.java
@@ -1,4 +1,4 @@
-package com.breaditnow;
+package com.breaditnow.mysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
