8bcc86aa060eccb5ae3887d0111c40bef560006a
pug9483 <2019136060@koreatech.ac.kr>
2025-02-12 00:04:36 +0900

    feat : naver checkstyle 설정 (#3)

--- breaditnow-common/src/main/java/com/breaditnow/common/BreaditnowCommonApplication.java
+++ breaditnow-common/src/main/java/com/breaditnow/common/BreaditnowCommonApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BreaditnowCommonApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BreaditnowCommonApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BreaditnowCommonApplication.class, args);
+	}
 }
--- breaditnow-redis/src/main/java/com/breaditnow/redis/BreaditnowRedisApplication.java
+++ breaditnow-redis/src/main/java/com/breaditnow/redis/BreaditnowRedisApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BreaditnowRedisApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BreaditnowRedisApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BreaditnowRedisApplication.class, args);
+	}
 }
