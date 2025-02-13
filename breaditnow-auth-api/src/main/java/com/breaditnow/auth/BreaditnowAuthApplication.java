2870c3a5f8207cacf1af8d88bfe7398b3e28c402
Jimin <thswlals0219@gmail.com>
2025-02-13 21:27:09 +0900

    chore: prod yml 생성 (#9)

--- breaditnow-auth-api/src/main/java/com/breaditnow/auth/BreaditnowAuthApplication.java
+++ breaditnow-auth-api/src/main/java/com/breaditnow/auth/BreaditnowAuthApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
 	"com.breaditnow.auth",
-	"com.breaditnow.mysql"})
+	"com.breaditnow.mysql",
+	"com.breaditnow.redis"})
 public class BreaditnowAuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BreaditnowAuthApplication.class, args);
