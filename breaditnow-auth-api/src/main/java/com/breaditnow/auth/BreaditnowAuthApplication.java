ae06c074641e6d1a3173906e9e8c34f0ffa813e2
Jimin <thswlals0219@gmail.com>
2025-02-13 19:45:25 +0900

    chore: common 애플리케이션 삭제 (#9)

--- breaditnow-auth-api/src/main/java/com/breaditnow/auth/BreaditnowAuthApplication.java
+++ breaditnow-auth-api/src/main/java/com/breaditnow/auth/BreaditnowAuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
 	"com.breaditnow.auth",
-	"com.breaditnow.redis"})
+	"com.breaditnow.mysql"})
 public class BreaditnowAuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BreaditnowAuthApplication.class, args);
