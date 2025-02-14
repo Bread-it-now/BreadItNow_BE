4fa1b221afde9bfe7ddedd7e9d9c96df894bd92c
Jimin <thswlals0219@gmail.com>
2025-02-14 17:01:20 +0900

    feat: api 예외 응답 포맷 구현 (#8)

---
+++ breaditnow-common/src/main/java/com/breaditnow/common/response/ApiErrorResponse.java
@@ -0,0 +1,26 @@
+package com.breaditnow.common.response;
+
+import static com.breaditnow.common.response.ResponseStatus.ERROR;
+
+import org.springframework.lang.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"status", "code", "message", "errors"})
+public record ApiErrorResponse(
+	ResponseStatus status,
+	String code,
+	String message,
+	@Nullable ErrorDetail errors)  {
+
+	public ApiErrorResponse(String code, String message, ErrorDetail errors) {
+		this(ERROR, code, message, errors);
+	}
+
+	public static ApiErrorResponse of(String code, String message, ErrorDetail errors) {
+		return new ApiErrorResponse(code, message, errors);
+	}
+}
+
--- breaditnow-common/src/main/java/com/breaditnow/common/response/ApiSuccessResponse.java
+++ breaditnow-common/src/main/java/com/breaditnow/common/response/ApiSuccessResponse.java
@@ -11,24 +11,24 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({"status", "data"})
-public record SuccessResponse<T>(
+public record ApiSuccessResponse<T>(
 	ResponseStatus status,
 	@Nullable T data)  {
 
-	public SuccessResponse(T data) {
+	public ApiSuccessResponse(T data) {
 		this(SUCCESS, data);
 	}
 
-	public static <T> SuccessResponse<T> of() {
-		return new SuccessResponse<>(null);
+	public static <T> ApiSuccessResponse<T> of() {
+		return new ApiSuccessResponse<>(null);
 	}
 
-	public static <T> SuccessResponse<Map<String, T>> of(String key, T data) {
-		return new SuccessResponse<>(Map.of(key, data));
+	public static <T> ApiSuccessResponse<Map<String, T>> of(String key, T data) {
+		return new ApiSuccessResponse<>(Map.of(key, data));
 	}
 
-	public static <T> SuccessResponse<T> of(T data) {
-		return new SuccessResponse<>(data);
+	public static <T> ApiSuccessResponse<T> of(T data) {
+		return new ApiSuccessResponse<>(data);
 	}
 
 }
