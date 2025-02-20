311d22d439b5ec5d8e44f672708145e6687af805
Jimin <thswlals0219@gmail.com>
2025-02-20 15:55:50 +0900

    feat: 소셜 로그인 시큐리티 config 설정 (#16)

---
+++ breaditnow-auth-api/src/main/java/com/breaditnow/auth/global/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.breaditnow.auth.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.breaditnow.auth.global.security.oauth2.service.CustomOAuth2UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private static final String[] AUTH = {"/oauth2/**", "/oauth/callback/**"};
+
+	private final CustomOAuth2UserService oauth2UserService;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.oauth2Login(oauth2 -> oauth2
+					.authorizationEndpoint(authorization -> authorization
+						.baseUri("/oauth2/authorization"))
+					.redirectionEndpoint(redirection -> redirection
+						.baseUri("/oauth/callback/*"))
+					.userInfoEndpoint(userInfo -> userInfo
+						.userService(oauth2UserService))
+			);
+
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers(AUTH).permitAll()
+				.anyRequest().authenticated()
+		);
+
+		return http.build();
+	}
+
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("https://localhost:9000"));
+		config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"));
+		config.setAllowedHeaders(List.of("Authorization", "Set-Cookie", "Content-Type"));
+		config.setExposedHeaders(List.of("Authorization", "Set-Cookie"));
+		config.setAllowCredentials(true);
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+}
---
+++ breaditnow-auth-api/src/main/java/com/breaditnow/auth/global/security/oauth2/service/CustomOAuth2UserService.java
@@ -0,0 +1,25 @@
+package com.breaditnow.auth.global.security.oauth2.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+		log.info("getAttributes : {}", oAuth2User.getAttributes());
+
+		String provider = userRequest.getClientRegistration().getRegistrationId();
+		log.info("provider : {}", provider);
+
+		return oAuth2User;
+	}
+}
