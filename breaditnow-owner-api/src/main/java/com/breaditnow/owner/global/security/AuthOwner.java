ed4ec8682be75649d618ec470d95b092a29df3e9
Jimin <thswlals0219@gmail.com>
2025-02-27 14:21:56 +0900

    feat: WebConfig에 OwnerArgumentResolver 등록 (#22)

--- breaditnow-owner-api/src/main/java/com/breaditnow/owner/global/security/AuthOwner.java
+++ breaditnow-owner-api/src/main/java/com/breaditnow/owner/global/security/AuthOwner.java
@@ -5,11 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-
 @Target({ElementType.PARAMETER, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : owner")
 public @interface AuthOwner {
 	boolean required() default true;
 }
