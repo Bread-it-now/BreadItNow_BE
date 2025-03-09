03169a51f9a9334e3314a8e2f09bccbf2ced31bb
Jimin <thswlals0219@gmail.com>
2025-03-09 16:26:22 +0900

    feat: 빵 카테고리도 저장 로직 분리 (#25)

--- breaditnow-owner-api/src/main/java/com/breaditnow/owner/domain/product/service/ProductService.java
+++ breaditnow-owner-api/src/main/java/com/breaditnow/owner/domain/product/service/ProductService.java
@@ -6,11 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.breaditnow.domain.domain.bakery.entity.Bakery;
 import com.breaditnow.domain.domain.bakery.repository.BakeryRepository;
-import com.breaditnow.domain.domain.product.entity.BreadCategory;
 import com.breaditnow.domain.domain.product.entity.Product;
-import com.breaditnow.domain.domain.product.entity.ProductBreadCategory;
-import com.breaditnow.domain.domain.product.repository.BreadCategoryRepository;
-import com.breaditnow.domain.domain.product.repository.ProductBreadCategoryRepository;
 import com.breaditnow.domain.domain.product.repository.ProductRepository;
 import com.breaditnow.owner.domain.product.controller.req.ProductCreateRequest;
 import com.breaditnow.owner.global.s3.upload.FileUploader;
@@ -25,8 +21,7 @@ public class ProductService {
 	private final BakeryRepository bakeryRepository;
 	private final ProductRepository productRepository;
 	private final FileUploader uploader;
-	private final BreadCategoryRepository categoryRepository;
-	private final ProductBreadCategoryRepository productBreadCategoryRepository;
+	private final ProductBreadCategoryService productBreadCategoryService;
 
 	@Transactional
 	public Long createProduct(Long ownerId, Long bakeryId, ProductCreateRequest request, MultipartFile productImage) {
@@ -38,21 +33,10 @@ public class ProductService {
 
 		Long[] breadCategoryIds = request.breadCategoryIds();
 		if (breadCategoryIds != null) {
-			addProductBreadCategories(breadCategoryIds, savedProduct);
+			productBreadCategoryService.addProductBreadCategories(breadCategoryIds, savedProduct);
 		}
 
 		return savedProduct.getId();
-	}
-
-	private void addProductBreadCategories(Long[] breadCategoryIds, Product savedProduct) {
-		for (Long breadCategoryId : breadCategoryIds) {
-			BreadCategory breadCategory = categoryRepository.getById(breadCategoryId);
-			ProductBreadCategory productBreadCategory = ProductBreadCategory.builder()
-				.breadCategory(breadCategory)
-				.product(savedProduct)
-				.build();
-			productBreadCategoryRepository.save(productBreadCategory);
-		}
 	}
 
 	private String uploadFile(MultipartFile file, String path) {
