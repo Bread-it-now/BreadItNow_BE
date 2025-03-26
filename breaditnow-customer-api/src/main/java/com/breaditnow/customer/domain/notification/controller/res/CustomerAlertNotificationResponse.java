8a33341ebd05b86a09a86fcd463141b64efba9e0
pug9483 <2019136060@koreatech.ac.kr>
2025-03-26 10:20:34 +0900

    refactor : 날짜 및 시간 변환 애노테이션 추가 (#47)

--- breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/notification/controller/res/CustomerAlertNotificationResponse.java
+++ breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/notification/controller/res/CustomerAlertNotificationResponse.java
@@ -1,11 +1,13 @@
 package com.breaditnow.customer.domain.notification.controller.res;
 
 import static com.breaditnow.domain.domain.notification.enumerate.NotificationType.*;
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.*;
 
 import java.time.LocalDateTime;
 
 import com.breaditnow.domain.domain.notification.entity.CustomerAlertNotification;
 import com.breaditnow.domain.domain.notification.enumerate.NotificationType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Builder;
 
@@ -20,6 +22,7 @@ public record CustomerAlertNotificationResponse(
 	Integer remainingCount,
 	Integer alertCount,
 	Boolean isRead,
+	@JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	LocalDateTime createdAt
 ) implements CustomerNotificationResponse {
 	public static CustomerNotificationResponse of(CustomerAlertNotification alertNotification) {
