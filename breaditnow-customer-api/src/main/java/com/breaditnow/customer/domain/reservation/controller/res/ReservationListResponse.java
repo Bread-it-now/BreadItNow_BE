f5867b6f17124b784367f9593fa4bfe0e2cfeed0
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-03-27 16:18:03 +0900

    feat : 총 예약상품 종류, 대표 빵 이름 프로퍼티를 추가 (#56)

--- breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/reservation/controller/res/ReservationListResponse.java
+++ breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/reservation/controller/res/ReservationListResponse.java
@@ -1,8 +1,10 @@
 package com.breaditnow.customer.domain.reservation.controller.res;
 
 import com.breaditnow.domain.domain.reservation.entity.Reservation;
+import com.breaditnow.domain.domain.reservation.entity.ReservationProduct;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record ReservationListResponse(
         Long reservationId,
@@ -14,9 +16,15 @@ public record ReservationListResponse(
         String bakeryName,
         int totalPrice,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        LocalDateTime pickupDeadline
+        LocalDateTime pickupDeadline,
+        Integer totalReservationProducts,
+        String mainReservationProductName
 ) {
     public static ReservationListResponse of(Reservation reservation) {
+        List<ReservationProduct> products = reservation.getReservationProducts();
+        int totalCount = products.stream().mapToInt(ReservationProduct::getQuantity).sum();
+        String mainName = products.isEmpty() ? null : products.get(0).getProductName();
+
         return new ReservationListResponse(
                 reservation.getId(),
                 reservation.getCreatedAt(),
@@ -25,7 +33,9 @@ public record ReservationListResponse(
                 reservation.getBakery().getId(),
                 reservation.getBakery().getName(),
                 reservation.getTotalPrice(),
-                reservation.getPickupDeadline()
+                reservation.getPickupDeadline(),
+                totalCount,
+                mainName
         );
     }
 }
