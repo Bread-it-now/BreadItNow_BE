81ea49b51874dfd4a17916c58f5cc0a63fe35dd5
pug9483 <2019136060@koreatech.ac.kr>
2025-03-30 21:03:31 +0900

    refactor : 페이지네이션 서비스 계층 분리 (#66)

--- breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/notification/service/NotificationService.java
+++ breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/notification/service/NotificationService.java
@@ -1,15 +1,11 @@
 package com.breaditnow.customer.domain.notification.service;
 
-import java.util.List;
 import java.util.function.Consumer;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.breaditnow.customer.domain.notification.controller.res.NotificationPageResponse;
 import com.breaditnow.domain.domain.notification.entity.CustomerNotification;
-import com.breaditnow.domain.domain.notification.enumerate.NotificationType;
 import com.breaditnow.domain.domain.notification.repository.UnifiedNotificationRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -31,13 +27,7 @@ public class NotificationService {
 		updateNotification(customerId, notificationId, notification -> notification.changeIsActive(false));
 		return notificationId;
 	}
-
-	public NotificationPageResponse getNotifications(Long customerId, Pageable pageable,
-		List<NotificationType> notificationTypes) {
-		return NotificationPageResponse.of(
-			unifiedNotificationRepository.find(customerId, pageable, notificationTypes));
-	}
-
+	
 	private void updateNotification(Long customerId, Long notificationId, Consumer<CustomerNotification> updater) {
 		CustomerNotification notification = unifiedNotificationRepository.getByCustomerIdAndId(customerId,
 			notificationId);
