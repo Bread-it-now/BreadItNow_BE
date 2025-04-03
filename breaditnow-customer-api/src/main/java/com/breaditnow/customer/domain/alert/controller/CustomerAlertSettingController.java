8a649ba85ef026da5cc5f8a4a0199da366329773
Taeyeong Kim <97294927+tyeong1102@users.noreply.github.com>
2025-04-03 17:21:34 +0900

    feat : 방해 금지 모드 설정 및 조회 구현 (#71)

---
+++ breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/alert/controller/CustomerAlertSettingController.java
@@ -0,0 +1,35 @@
+package com.breaditnow.customer.domain.alert.controller;
+
+import com.breaditnow.common.response.ApiSuccessResponse;
+import com.breaditnow.customer.domain.alert.controller.req.CustomerDoNotDisturbUpdateRequest;
+import com.breaditnow.customer.domain.alert.controller.res.CustomerDoNotDisturbResponse;
+import com.breaditnow.customer.domain.alert.service.CustomerAlertSettingService;
+import com.breaditnow.customer.global.security.annotation.AuthCustomer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/alert")
+@Slf4j
+public class CustomerAlertSettingController {
+
+    private final CustomerAlertSettingService customerAlertSettingService;
+
+    @GetMapping("/do-not-disturb")
+    public ApiSuccessResponse<CustomerDoNotDisturbResponse> getDoNotDisturbSetting(
+            @AuthCustomer Long customerId
+    ) {
+        return ApiSuccessResponse.of(customerAlertSettingService.getDoNotDisturbSetting(customerId));
+    }
+
+    @PutMapping("/do-not-disturb")
+    public ApiSuccessResponse<Void> updateDoNotDisturbSetting(
+            @AuthCustomer Long customerId,
+            @RequestBody CustomerDoNotDisturbUpdateRequest request
+    ) {
+        customerAlertSettingService.updateDoNotDisturbSetting(customerId, request);
+        return ApiSuccessResponse.of();
+    }
+}
--- breaditnow-domain/src/main/java/com/breaditnow/domain/domain/alert/repository/CustomerAlertSettingRepository.java
+++ breaditnow-domain/src/main/java/com/breaditnow/domain/domain/alert/repository/CustomerAlertSettingRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.breaditnow.domain.domain.alert.entity.CustomerAlertSetting;
 
+import java.util.Optional;
+
 public interface CustomerAlertSettingRepository extends JpaRepository<CustomerAlertSetting, Long> {
 
+    Optional<CustomerAlertSetting> findByCustomerId(Long customerId);
 }
 
