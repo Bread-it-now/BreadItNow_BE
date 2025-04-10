76727b7acb22ed5ec779bc53e3c70368f08e39a7
pug9483 <2019136060@koreatech.ac.kr>
2025-04-10 16:36:07 +0900

    docs : swagger customer api sample 추가 (#70)

--- breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/reservation/controller/req/ReservationRequest.java
+++ breaditnow-customer-api/src/main/java/com/breaditnow/customer/domain/reservation/controller/req/ReservationRequest.java
@@ -1,11 +1,19 @@
 package com.breaditnow.customer.domain.reservation.controller.req;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
 public record ReservationRequest(
-        @NotNull Long bakeryId,
-        @NotNull List<ReservationProductRequest> reservationProducts
+	@Schema(description = "예약할 빵집의 ID", example = "101")
+	@NotNull Long bakeryId,
+
+	@NotNull
+	@Schema(
+		description = "예약할 상품 목록. 각 항목은 예약할 상품의 상세 정보를 포함합니다.",
+		example = "[{ \"productId\": 201, \"quantity\": 2 }]"
+	)
+	List<ReservationProductRequest> reservationProducts
 ) {
 }
